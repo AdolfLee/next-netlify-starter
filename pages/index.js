3a70f21948a235305b23cb5d15bcec1f977cdc56
AdolfLee <140749879+AdolfLee@users.noreply.github.com>
2023-07-27 21:15:07 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,18 +5,12 @@ import Footer from '@components/Footer'
 export default function Home() {
   return (
     <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
       <main>
         <Header title="Redirecting..." />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
       </main>
-
       <Footer />
     </div>
   )
